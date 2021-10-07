4e304df3ca2cd8dff2de21aee3523da976b48861
Mark A. Miller <mamillerpa@gmail.com>
2021-10-07 08:58:08 -0400

    proper basename import

--- util/make_wide_ha_chunks.py
+++ util/make_wide_ha_chunks.py
@@ -1,5 +1,5 @@
 from os import listdir
-from os.path import join
+from os.path import join, basename
 import pandas as pd
 import glob
 
@@ -14,7 +14,7 @@ for file in data_files:
     print(file)
 #    full_path = join(data_root, file)
     full_path = file
-    file = os.path.basename(full_path)
+    file = basename(full_path)
     print(full_path)
     print(file)
     long_chunk = pd.read_csv(full_path, sep="\t")
