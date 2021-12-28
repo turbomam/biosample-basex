12cc1c27dc8d8dd63e36242ea6997dd3a0479b5f
Mark Andrew Miller <MAM@lbl.gov>
2021-12-27 19:51:31 -0500

    still slow

--- util/extract_harmonizeds.py
+++ util/extract_harmonizeds.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 database_file = '../target/biosample_basex.db'
 # 320 054 875 rows about 24 341 464 biosamples
-chunk_size = 1000
+chunk_size = 10000
 
 conn = sqlite3.connect(database_file)
 
@@ -25,9 +25,11 @@ conn = sqlite3.connect(database_file)
 # print(aa_rc_res[0])
 
 aa_max_id_q = 'select max(cast(raw_id as int)) from all_attribs aa'
+print("counting biosamples")
+print(datetime.datetime.now())
 cursor = conn.execute(aa_max_id_q)
 aa_max_id_res = cursor.fetchone()[0]
-print(f"{aa_max_id_res} rows")
+print(f"{aa_max_id_res} biosamples")
 
 all_hns_q = '''
 select
@@ -40,6 +42,8 @@ where
 order by
 	harmonized_name'''
 
+print("determining attributes")
+print(datetime.datetime.now())
 all_hns_frame = pd.read_sql_query(all_hns_q, conn)
 all_hns = list(all_hns_frame['harmonized_name'])
 all_cols = all_hns
@@ -60,7 +64,8 @@ for start, stop in start_stops.items():
     print(f"{start} to {stop}")
     hns_rows_in_range_q = f'''
     select
-    	distinct raw_id, harmonized_name, GROUP_CONCAT(value, "|||") as value
+    --	distinct raw_id, harmonized_name, GROUP_CONCAT(value, "|||") as value
+    raw_id, harmonized_name, value
     from
     	all_attribs aa
     where
@@ -68,11 +73,22 @@ for start, stop in start_stops.items():
     	and raw_id > {start}
     	and raw_id < {stop}
     	group by raw_id, harmonized_name'''
+    print("starting query")
+    print(datetime.datetime.now())
     hns_rows_in_range_frame = pd.read_sql_query(hns_rows_in_range_q, conn)
+    print("query done")
+    print(datetime.datetime.now())
 
     pivoted_in_range = hns_rows_in_range_frame.pivot(index="raw_id", columns="harmonized_name", values="value")
+    print("pivot done")
+    print(datetime.datetime.now())
 
     harmonized_wide = pd.concat([empty_frame, pivoted_in_range])
+    print("frame concatenation done")
+    print(datetime.datetime.now())
+
     harmonized_wide["raw_id"] = harmonized_wide.index
 
     harmonized_wide.to_sql("harmonized_wide", conn, index=False, if_exists="append")
+    print("insertion back into SQLite done")
+    print(datetime.datetime.now())
