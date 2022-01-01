9722bcb5656166b18e383eaf83124f80ce2dd92a
Mark Andrew Miller <MAM@lbl.gov>
2022-01-01 11:15:42 -0500

    investigate_unharmonized progress

--- util/investigate_unharmonized.py
+++ util/investigate_unharmonized.py
@@ -1,37 +1,42 @@
 import sqlite3
 import datetime
 import pandas as pd
+import click
 
-database_file = "target/biosample_basex.db"
-# "../target/biosample_basex.db"
-pattern = '%grow%facil%'
-output_file = 'target/investigate_unharmonized.tsv'
 
-conn = sqlite3.connect(database_file)
+# , help='attribute name patten using SQL %,_, etc.')
+@click.command()
+@click.option('--pattern', required=True, help='attribute name patten using SQL %,_, etc.')
+@click.option('--database_file', type=click.Path(exists=True), default="target/biosample_basex.db", show_default=True)
+@click.option('--output_file', type=click.Path(), default="target/investigate_unharmonized.tsv", show_default=True)
+def investigate_unharmonized(pattern, database_file, output_file):
+    """Find rows in all_attribs where the attribute_name matches the pattern."""
+    conn = sqlite3.connect(database_file)
 
-pattern_query = f"""
-select
-	aa.raw_id ,
-	hwr.env_package,
-	aa.attribute_name ,
-	aa.harmonized_name ,
-	aa.value
-from
-	all_attribs aa
-join harmonized_wide_repaired hwr 
-	on
-	aa.raw_id = hwr.raw_id
-where
-	lower(attribute_name) like '{pattern}'
-;
-"""
+    pattern_query = f"""
+    select
+    	aa.raw_id ,
+    	hwr.env_package,
+    	aa.attribute_name ,
+    	aa.harmonized_name ,
+    	aa.value
+    from
+    	all_attribs aa
+    join harmonized_wide_repaired hwr 
+    	on
+    	aa.raw_id = hwr.raw_id
+    where
+    	lower(attribute_name) like '{pattern}'
+    ;
+    """
 
-# print(pattern_query)
+    # 2022-01-01 about 6 minutes
+    print(datetime.datetime.now())
+    patterns_res_frame = pd.read_sql_query(pattern_query, conn)
+    print(datetime.datetime.now())
 
-print(datetime.datetime.now())
-patterns_res_frame = pd.read_sql_query(pattern_query, conn)
-print(datetime.datetime.now())
+    patterns_res_frame.to_csv(output_file, sep="\t", index=False)
 
-# print(patterns_res_frame)
 
-patterns_res_frame.to_csv(output_file, sep="\t", index=False)
+if __name__ == '__main__':
+    investigate_unharmonized()
