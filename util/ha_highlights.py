3317f75e020a96baaed87d42930312865341400c
Mark Andrew Miller <MAM@lbl.gov>
2022-01-02 14:18:10 -0500

    harm attrib reports for MS

--- util/ha_highlights.py
+++ util/ha_highlights.py
@@ -7,8 +7,9 @@ import click
 # , help='attribute name patten using SQL %,_, etc.')
 @click.command()
 @click.option('--database_file', type=click.Path(exists=True), default="target/biosample_basex.db", show_default=True)
-@click.option('--output_file', type=click.Path(), default="target/ha_highlights.tsv", show_default=True)
-def ha_highlights(database_file, output_file):
+# @click.option('--output_file', type=click.Path(), default="target/ha_highlights.tsv", show_default=True)
+@click.option('--output_dir', type=click.Path(), default="reports", show_default=True)
+def ha_highlights(database_file, output_dir):
     """Tabulate the values of some attributes with harmonized names by environmental package"""
     conn = sqlite3.connect(database_file)
 
@@ -38,17 +39,27 @@ def ha_highlights(database_file, output_file):
 
     # 2022-01-01 about 10 minutes
     print(datetime.datetime.now())
-    overall_res = pd.read_sql_query(overall_query, conn)
+    highlights_frame = pd.read_sql_query(overall_query, conn)
     print(datetime.datetime.now())
 
-    # # print(overall_res)
-    #
-    # subset = overall_res[["env_package", ""]]
-    # print(overall_res)
-    #
-    # # pivoted = overall_res.pivot(index="", columns="", values=)
+    # overall_res.to_csv(highlights_frame, sep="\t", index=False)
 
-    overall_res.to_csv(output_file, sep="\t", index=False)
+    hfc = list(highlights_frame.columns)
+    hfc.remove("raw_id")
+    hfc.remove("env_package")
+    hfc.sort()
+
+    for i in hfc:
+        print(i)
+        # use path join?
+        output_file = f"{output_dir}/{i}_by_env_package.tsv"
+        by_env_pack = pd.DataFrame(
+            highlights_frame.value_counts(["env_package", i], dropna=False)
+        ).reset_index()
+        by_env_pack.rename(columns={0: "count"}, inplace=True)
+        #     print(by_env_pack)
+        print(output_file)
+        by_env_pack.to_csv(output_file, sep="\t", index=False)
 
 
 if __name__ == '__main__':
